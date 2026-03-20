859c0b763ca8d9e17cdf053844bab2a404053948
Hardik Vijay <hardikvijay@Hardiks-MacBook-Pro.local>
2026-03-20 11:41:42 +0530

    Configure GitHub Pages basePath

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/CRM-vijayvergia-and-sons",
-  assetPrefix: "/CRM-vijayvergia-and-sons/",
-  /* config options here */
+  basePath: process.env.GITHUB_PAGES ? "/CRM-vijayvergia-and-sons" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
